a11668b5fc856507735d1f9c82a50e8f8f54003b
Oliver Boudet <oboudet1@me.com>
2019-02-13 09:17:40 +0100

    add relevant files

---
+++ src/Documentation/Interfaces/ContorllerInfo.ts
@@ -0,0 +1,28 @@
+interface IParameter {
+    Name: string;
+    Type: "string" | "number";
+    Example: string;
+}
+
+interface IRequest {
+    Endpoint: string;
+    Method: "POST" | "GET" | "PUT" | "PATCH" | "DELETE";
+    Parameters: IParameter[];
+}
+
+interface IResponse {
+    Redirect?: string;
+    Body?: any;
+}
+
+export interface IControllerInfo {
+    Name: string;
+    Category: string;
+    Description: string;
+    Request: IRequest;
+    Response: IResponse;
+}
+
+export interface IControllers {
+    Controllers: IControllerInfo[];
+}
---
+++ src/Documentation/Utilities/GetDocs.ts
@@ -0,0 +1,78 @@
+import { IControllerInfo } from "@docs/Interfaces/ContorllerInfo";
+import {
+    readdirSync,
+    statSync,
+    existsSync,
+    mkdirSync,
+    readFileSync,
+    writeFileSync
+} from "fs";
+import { join } from "path";
+import ejs from "ejs";
+
+export async function GetDocsInfo(path: string): Promise<IControllerInfo[]> {
+    const docs: IControllerInfo[] = [];
+    const files = walkSync(path);
+
+    for (const file of files) {
+        const x = await import(file);
+        if (x.Info) {
+            docs.push(x.Info);
+        }
+    }
+    LoadAndCompileDocs(docs);
+
+    return docs;
+}
+
+async function LoadAndCompileDocs(docs: IControllerInfo[]) {
+    const buildPath = join(__dirname, "../build");
+    const templatePath = join(__dirname, "../../../views/docTemplate.ejs");
+    const writePath = join(__dirname, "../../../public/doc.html");
+
+    if (!existsSync(templatePath)) {
+        throw new Error("No documentation template file found...");
+    }
+    if (!existsSync(buildPath)) mkdirSync(buildPath);
+
+    // const template = ejs.compile(readFileSync(templatePath));
+    const file = readFileSync(templatePath, "utf8");
+    console.log(docs);
+
+    const template = ejs.render(file, {
+        docs,
+        categories: GetUniqueCategories(docs)
+    });
+
+    writeFileSync(writePath, template, "utf8");
+    // template({ docs });
+}
+
+function GetUniqueCategories(docs: IControllerInfo[]): string[] {
+    const categories: string[] = [];
+    for (const doc of docs) {
+        if (!categories.includes(doc.Category)) {
+            categories.push(
+                `${doc.Category.charAt(0).toUpperCase()}${doc.Category.slice(
+                    1
+                )}`
+            );
+        }
+    }
+    return categories;
+}
+
+const walkSync = (path: string, filelist?) => {
+    const files = readdirSync(path);
+    filelist = filelist || [];
+    files.forEach((file) => {
+        if (statSync(join(path, file)).isDirectory()) {
+            filelist = walkSync(join(path, file), filelist);
+        } else {
+            if (!file.includes("map")) {
+                filelist.push(join(path, file));
+            }
+        }
+    });
+    return filelist;
+};
